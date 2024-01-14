da71c413d4204ae719fbf3aaace8c19ee1dfd5b8
runehero <runehero@hotmail.com>
2024-01-14 20:30:48 +0100

    DataTransferHelper: * New class to transfer property values between classes.
    PasswordHelper:
    * New class to create hashed passwords.

---
+++ FribergCarRentals/Data/DataTransferHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FribergCarRentals.Data
+{
+    internal static class DataTransferHelper
+    {
+        public static bool TryTransferData<TSource, TDest>(TSource sourceObject, [NotNullWhen(returnValue: true)] out TDest destinationObject) 
+            where TSource : class where TDest : class, new()
+        {
+            destinationObject = new TDest();
+            var sourceProperties = typeof(TSource).GetProperties();
+            var destinationProperties = typeof(TDest).GetProperties();
+            bool dataWasWritten = false;
+
+            foreach (var sourceProperty in sourceProperties)
+            {
+                var destinationProperty = destinationProperties.FirstOrDefault(x => x.Name == sourceProperty.Name && x.PropertyType == sourceProperty.PropertyType && x.CanWrite);
+
+                if (destinationProperty != null)
+                {
+                    destinationProperty.SetValue(destinationObject, sourceProperty.GetValue(sourceObject));
+                    dataWasWritten = true;
+                }
+            }
+
+            return dataWasWritten;
+        }
+    }
+}
---
+++ FribergCarRentals/Data/PasswordHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FribergCarRentals.Data
+{
+    internal static class PasswordHelper
+    {
+        #region Methods
+
+        public static string CreateHashedPassword(string password)
+        {
+            var salt = new byte[16];
+
+            using (var random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(salt);
+            }
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(20);
+            var hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);        
+            string hashString = Encoding.UTF8.GetString(hashBytes, 0, hashBytes.Length);
+            return hashString;
+        }
+
+        #endregion
+    }
+}
