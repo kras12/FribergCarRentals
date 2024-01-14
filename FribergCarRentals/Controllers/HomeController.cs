1d672b813a63caa24a52ea9e0d6f110b91386fd8
runehero <runehero@hotmail.com>
2024-01-14 01:40:31 +0100

    HomeController.cs: * Tweaked routing template.

--- FribergCarRentals/Controllers/HomeController.cs
+++ FribergCarRentals/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace FribergCarRentals.Controllers
 {
-    [Route("")]
+    [Route("/[action]")]
     public class HomeController : Controller
     {
         private readonly ICarRepository _carRepository;
@@ -15,42 +15,30 @@ namespace FribergCarRentals.Controllers
             _carRepository = carRepository;
         }
 
-        [Route("")]
+        [Route("/")]
         public IActionResult Index()
         {
             return View();
         }
 
-        [Route("Privacy")]
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [Route("Error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        [Route("Cars")]
         public async Task<IActionResult> Cars()
         {
-            //var testCar = new CarEntity("Bmw", "Blue", "523", 2008, "ABC123", VehiclePropulsionEntity.CreateSeedObject(FribergCars.Shared.SharedTypes.VehiclePropulsionType.Gasoline), 
-            //    CarRentalStatusEntity.CreateSeedObject(FribergCars.Shared.SharedTypes.CarRentalStatus.Available));
-            //testCar.Images.Add(new ImageEntity(@"C:\test-image.jpg"));
-
             return View(await _carRepository.GetAll());
         }
 
-        [Route("Car/{id}")]
         public async Task<IActionResult> Car(int id)
         {
-            //var testCar = new CarEntity("Bmw", "Blue", "523", 2008, "ABC123", VehiclePropulsionEntity.CreateSeedObject(FribergCars.Shared.SharedTypes.VehiclePropulsionType.Gasoline),
-            //    CarRentalStatusEntity.CreateSeedObject(FribergCars.Shared.SharedTypes.CarRentalStatus.Available));
-            //testCar.Images.Add(new ImageEntity(@"C:\test-image.jpg"));
-
             var car = await _carRepository.GetById(id);
 
             if (car is not null)
