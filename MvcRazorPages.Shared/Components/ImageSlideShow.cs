27b0af5c967698b0c489a6dddacfaaedf9f0fc99
runehero <runehero@hotmail.com>
2024-06-21 18:52:33 +0200

    Moved the image slide show component to a shared project.

---
+++ MvcRazorPages.Shared/Components/ImageSlideShow.cs
@@ -0,0 +1,40 @@
+﻿using MvcRazorPages.Shared.ViewModels.Other;
+using Microsoft.AspNetCore.Mvc;
+using MvcRazorPages.Shared.ViewModels.Image;
+
+namespace MvcRazorPages.Shared.Components
+{
+	/// <summary>
+	/// A class that handles image slideshows.
+	/// </summary>
+	public class ImageSlideShow : ViewComponent
+	{
+		#region Methods
+
+		/// <summary>
+		/// The invoke method.
+		/// </summary>
+		/// <param name="images">The images to include in the slide show.</param>
+		/// <returns>A <see cref="Task{TResult}"/> containing an <see cref="IViewComponentResult"/>.</returns>
+		public IViewComponentResult Invoke(List<SlideShowImageViewModel> images)
+		{
+			#region Checks
+
+			if (images == null || images.Count == 0)
+			{
+				throw new ArgumentException("The image collection can't be empty.", nameof(images));
+			}
+
+			#endregion
+
+			if (images is null)
+			{
+				throw new ArgumentNullException(nameof(images));
+			}
+
+			return View(new ListViewModel<SlideShowImageViewModel>(images));
+		}
+
+		#endregion
+	}
+}
