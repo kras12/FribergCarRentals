c504d09decacd8c69342f60fd6b75b235b7893bb
runehero <runehero@hotmail.com>
2024-06-22 12:30:19 +0200

    Created a new shared project that contains applicatoin user roles.

---
+++ FribergCarRentals.Shared/Constants/ApplicationUserRoles.cs
@@ -0,0 +1,22 @@
+﻿namespace FribergFastigheter.Shared.Constants
+{
+    /// <summary>
+    /// A class that contains application user roles.
+    /// </summary>
+    public static class ApplicationUserRoles
+    {
+        #region Constants
+
+        /// <summary>
+        /// The broker admin role. 
+        /// </summary>
+        public const string Admin = "Admin";
+
+        /// <summary>
+        /// The broker role.
+        /// </summary>
+        public const string Member = "Member";
+
+        #endregion
+    }
+}
