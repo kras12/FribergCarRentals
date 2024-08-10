811b49be89218f9349743d7230ca88f11ad3e2c8
runehero <runehero@hotmail.com>
2024-08-10 20:27:56 +0200

    Blazor: Created a register customer button component to use in the navigation menu. It works simliar to the login button.

---
+++ FribergCarRentalsBlazor/Components/RegisterCustomerButton.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace FribergCarRentalsBlazor.Components
+{
+    /// <summary>
+    /// A component that handles both registration functionality for customers.
+    /// </summary>
+    public partial class RegisterCustomerButton : ComponentBase
+    {
+        #region Fields
+
+        /// <summary>
+        /// The id of the modal dialog. 
+        /// </summary>
+        private readonly string _modalDialogId = Guid.NewGuid().ToString();
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// HTML classes to use for the login button
+        /// </summary>
+        [Parameter]
+        public string RegisterButtonCssClasses { get; set; } = "";
+
+        /// <summary>
+        /// The injected JavaScript runtime. 
+        /// </summary>
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; } = default!;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Event handler for the registration successful event in the <see cref="RegisterCustomer"/> component.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing an async operation.</returns>
+        private async Task OnRegistrationSuccessful()
+        {
+            await JSRuntime.InvokeVoidAsync("HideCustomerRegistrationModal", _modalDialogId);
+        }      
+
+        #endregion
+    }
+}
