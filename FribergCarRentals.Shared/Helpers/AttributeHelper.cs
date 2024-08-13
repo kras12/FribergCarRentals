7e16a3a776b80f07adf2d72d49a0d55c6ca3c42d
runehero <runehero@hotmail.com>
2024-08-13 19:02:19 +0200

    New helper class for retrieving values of display name attributes.

---
+++ FribergCarRentals.Shared/Helpers/AttributeHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FribergCarRentals.Shared.Helpers
+{
+    /// <summary>
+    /// A helper class to manage attributes.
+    /// </summary>
+    public static class AttributeHelper
+    {
+        #region Methods
+
+        /// <summary>
+        /// Returns the value of a display attribute on a property if such attribute exists.
+        /// </summary>
+        /// <param name="expression">The expression for the property.</param>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <returns>A <see cref="string"/> that contains the value if the attribute exists, or the name of the property if it doesn't.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string GetDisplayName<TProperty>(Expression<Func<TProperty>> expression)
+        {
+            var memberExpression = (MemberExpression)expression.Body;
+            var value = memberExpression.Member.GetCustomAttribute(typeof(DisplayNameAttribute)) as DisplayNameAttribute;
+            return value?.DisplayName ?? memberExpression.Member.Name ?? throw new ArgumentException("Invalid expression", nameof(expression));
+        }
+
+        #endregion
+    }
+}
