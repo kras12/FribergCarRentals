2e1a0943c4395c27082d1fcbd88ef90ac357c998
runehero <runehero@hotmail.com>
2024-08-29 17:19:02 +0200

    ViewModels: Minor format change.

--- FribergCarRentals.Shared.Models/ViewModels/Order/CarBookingViewModel.cs
+++ FribergCarRentals.Shared.Models/ViewModels/Order/CarBookingViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
 using FribergCarRentals.Shared.Constants;
 using FribergCarRentals.Shared.Models.ViewModels.Car;
 using FribergCarRentals.Shared.Models.ViewModels.Other;
@@ -26,10 +25,22 @@ namespace FribergCarRentals.Shared.Models.ViewModels.Order
         [DisplayName("Booking ID")]
         public int CarBookingId { get; set; }
 
-        /// <summary>
-        /// The order the booking belongs to.
-        /// </summary>
-        [DisplayName("Order")]
+		/// <summary>
+		/// The number of rental days the customer is paying for. 
+		/// </summary>
+		[DisplayName("Debited Days")]
+		public int DebitedRentalDays
+		{
+			get
+			{
+				return (CarReturnDate - CarPickupDate).Days + 1;
+			}
+		}
+
+		/// <summary>
+		/// The order the booking belongs to.
+		/// </summary>
+		[DisplayName("Order")]
         public OrderViewModel Order { get; set; } = default!;
 
         /// <summary>
@@ -40,32 +51,20 @@ namespace FribergCarRentals.Shared.Models.ViewModels.Order
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = ValidationRules.DateFormatString)]
         public DateTime CarPickupDate { get; set; }
 
-        /// <summary>
-        /// The car return date.
-        /// </summary>
-        /// <remarks>The date is saved in the database with the time component stripped off.</remarks>
-        [DisplayName("Return Date")]
+		/// <summary>
+		/// The rental cost per day.
+		/// </summary>
+		[DisplayName("Cost Per Day")]
+		[DisplayFormat(DataFormatString = ValidationRules.DefaultPriceOutputFormatString)]
+		public decimal RentalCostPerDay { get; set; }
+
+		/// <summary>
+		/// The car return date.
+		/// </summary>
+		/// <remarks>The date is saved in the database with the time component stripped off.</remarks>
+		[DisplayName("Return Date")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = ValidationRules.DateFormatString)]
         public DateTime CarReturnDate { get; set; }
-
-        /// <summary>
-        /// The number of rental days the customer is paying for. 
-        /// </summary>
-        [DisplayName("Debited Days")]
-        public int DebitedRentalDays
-        {
-            get
-            {
-                return (CarReturnDate - CarPickupDate).Days + 1;
-            }
-        }
-
-        /// <summary>
-        /// The rental cost per day.
-        /// </summary>
-        [DisplayName("Cost Per Day")]
-        [DisplayFormat(DataFormatString = ValidationRules.DefaultPriceOutputFormatString)]
-        public decimal RentalCostPerDay { get; set; }
 
         #endregion
     }
